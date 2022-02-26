6fa357a31a46eaf0bedbdcde128bd9a0a4a9f9f9
jbucanay <gilbert.baracka@gmail.com>
2022-02-25 21:30:21 -0700

    initial

---
+++ cardlist/card-list.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="cardlist.css">
+    <title>Air Asia | Card List</title>
+</head>
+<body >
+    <br>
+    
+    <br>
+    <div class="btn-group">
+        <a href="../addcustomer/addcustomer.htm" class="btn btn-primary" aria-current="page">Add Customer</a>
+        <pre>Next card</pre>
+        <a href="../addcard/addcard.htm" class="btn btn-primary">Add Card</a>
+      </div>
+    <br>
+    <div id="cardlist">
+        <a href="../carddetails/card-details.htm">
+    <div class="card" style="width: 18rem;" class="card">
+        <img src="../images/gift-card.png" class="card-img-top" alt="gift card">
+        <div class="card-body">
+          <h5 class="card-title">Preferred</h5>
+          <p class="card-text">50 points</p>
+        </div>
+      </div></a>
+      <a href="../carddetails/card-details.htm">
+      <div class="card" style="width: 18rem;" class="card">
+        <img src="../images/gift-card.png" class="card-img-top" alt="gift card">
+        <div class="card-body">
+          <h5 class="card-title">Gift of love</h5>
+          <p class="card-text">30 points</p>
+        </div>
+      </div></a>
+      <a href="../carddetails/card-details.htm">
+      <div class="card" style="width: 18rem;" class="card">
+        <img src="../images/gift-card.png" class="card-img-top" alt="gift card">
+        <div class="card-body">
+          <h5 class="card-title">Show appreciation</h5>
+          <p class="card-text">20 points</p>
+        </div>
+      </div></a>
+      <a href="../carddetails/card-details.htm">
+      <div class="card" style="width: 18rem;" class="card">
+        <img src="../images/gift-card.png" class="card-img-top" alt="gift card">
+        <div class="card-body">
+          <h5 class="card-title">Premium</h5>
+          <p class="card-text">100 points</p>
+        </div>
+      </div> </a>
+      <a href="../carddetails/card-details.htm">
+      <div class="card" style="width: 18rem;" class="card">
+        <img src="../images/gift-card.png" class="card-img-top" alt="gift card">
+        <div class="card-body">
+          <h5 class="card-title">Gold card</h5>
+          <p class="card-text">200 points</p>
+        </div>
+      </div></a>
+      <a href="../carddetails/card-details.htm">
+      <div class="card" style="width: 18rem;" class="card">
+        <img src="../images/gift-card.png" class="card-img-top" alt="gift card">
+        <div class="card-body">
+          <h5 class="card-title">Members card</h5>
+          <p class="card-text">300 points</p>
+        </div>
+      </div></a>
+      <a href="../carddetails/card-details.htm">
+      <div class="card" style="width: 18rem;" class="card">
+        <img src="../images/gift-card.png" class="card-img-top" alt="gift card">
+        <div class="card-body">
+          <h5 class="card-title">Standard</h5>
+          <p class="card-text">400 points</p>
+        </div>
+      </div></a>
+      
+    </div>
+</body>
+</html>
